2835640e640ec8e470315faaa0949c0b01eea820
David Solís <dsolis1@educaib.eu>
2018-11-16 12:24:55 +0100

    Adaptació a la funció php.

--- Tramits/Certificats/form-to-email.php
+++ Tramits/Certificats/form-to-email.php
@@ -4,12 +4,13 @@ if(!isset($_POST['submit']))
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$Nom_Sollicitant = $_POST['Nom_Sollicitant'];
+$email_Sollicitant = $_POST['email_Sollicitant'];
+$DNI_Sollicitant = $_POST['DNI_Sollicitant'];
+$Obervacions_Sollicitant = $_POST['Obervacions_Sollicitant'];
 
 //Validate first
-if(empty($name)||empty($visitor_email)) 
+/*if(empty($name)||empty($visitor_email)) 
 {
     echo "Name and email are mandatory!";
     exit;
@@ -19,20 +20,20 @@ if(IsInjected($visitor_email))
 {
     echo "Bad email value!";
     exit;
-}
+}*/
 
-$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_from = 'secretariaeoimanacor@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+$email_body = "You have received a new message from the user $Nom_Sollicitant.\n".
+    "Here is the message:\n $DNI_Sollicitant".
     
-$to = "tom@amazing-designs.com";//<== update the email address
+$to = "secretariaeoimanacor@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $email_Sollicitant \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: thank-you.html');
+header('Location: https://www.eoimanacor.com/?page_id=5028&preview=true');
 
 
 // Function to validate against any email injection attempts
