f092e3a63d007f66764ce08e982284ad5386a452
David Solís <dsolis1@educaib.eu>
2019-03-21 19:38:50 +0100

    Inclusió de comentaris.

--- Matrícula oficial 2019-20/procesAdmissio.js
+++ Matrícula oficial 2019-20/procesAdmissio.js
@@ -1,7 +1,13 @@
+
+// Funció que regula el globus amb l'horari d'atenció al públic
+
 function mostraAmagaGlobus(lloc, boto){
+	
+	// Mostram/amagam el globus
 	x=document.getElementById(lloc);
 	x.classList.toggle('amagat');
 
+	// Posam el botó que ha disparat l'event de color gris/blanc
 	y=document.getElementById(boto);
 	y.classList.toggle('botoGris');
 }
