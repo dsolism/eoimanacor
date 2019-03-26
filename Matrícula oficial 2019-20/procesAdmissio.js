0b75cbbc15d51f5943a9e4f413fcbc0f68a130a7
David Solís <dsolis1@educaib.eu>
2019-03-26 18:21:35 +0100

    Adaptació a WordPress. Animació afegida al globus d'atenció al públic.

--- Matrícula oficial 2019-20/procesAdmissio.js
+++ Matrícula oficial 2019-20/procesAdmissio.js
@@ -5,7 +5,7 @@ function mostraAmagaGlobus(lloc, boto){
 	
 	// Mostram/amagam el globus
 	x=document.getElementById(lloc);
-	x.classList.toggle('amagat');
+	x.classList.toggle('globusVisible');
 
 	// Posam el botó que ha disparat l'event de color gris/blanc
 	y=document.getElementById(boto);
